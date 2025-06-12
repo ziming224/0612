0b258e06084be62d404be7ca025b4aae7701e83f
ziming <s11400156316006@gmail.com>
2025-06-12 14:05:58 +0800

    修icon

--- vite.config.mjs
+++ vite.config.mjs
@@ -38,19 +38,19 @@ export default defineConfig({
         short_name: 'Pomodoro',
         icons: [
           {
-            src: '/web-app-manifest-144x144.png',
+            src: './web-app-manifest-144x144.png',
             sizes: '144x144',
             type: 'image/png',
             purpose: 'any',
           },
           {
-            src: '/web-app-manifest-192x192.png',
+            src: './web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable',
           },
           {
-            src: '/web-app-manifest-512x512.png',
+            src: './web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable',
